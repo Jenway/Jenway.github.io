dcf4646b325748c8904a295f07f83039f46ec520
Jenway <JenwayHere@Gmail.com>
2023-02-11 08:33:20 +0000

    second try Updated By Github Actions

--- js/aplayer.js
+++ js/aplayer.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><meting-js
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><meting-js
     server="netease"
     type="song"
     autoplay="true"
