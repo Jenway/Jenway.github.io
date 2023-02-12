8a8371ad9266018becf03b408faa4a3bcff31154
Jenway <JenwayHere@Gmail.com>
2023-02-12 13:44:25 +0000

    增加了badge Updated By Github Actions

--- js/aplayer.js
+++ js/aplayer.js
@@ -1,7 +1,24 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><meting-js
-    server="netease"
-    type="song"
-    autoplay="true"
-    fixed="true"
-    id="25704085"
-</meting-js>
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>!(function() {
+  var oldLoadAp = window.onload;
+  window.onload = function () {
+    oldLoadAp && oldLoadAp();
+
+    new APlayer({
+      container: document.getElementById('aplayer'),
+      fixed: true,
+      autoplay: false,
+      loop: 'all',
+      order: 'random',
+      theme: '#b7daff',
+      preload: 'none',
+      audio: [
+        {
+          name: 'song1',
+          artist: 'artist1',
+          url: 
+          cover: 
+        }
+      ]
+    });
+  }
+})();
--- js/head.js
+++ js/head.js
@@ -1,25 +1,14 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script type="text/javascript">
-      /*  动态标题*/
-     var OriginTitile = document.title,
-      titleTime;
-     document.addEventListener("visibilitychange",
-     function() {
-      if (document.hidden) {
-        document.title = "你别走吖 Σ(っ °Д °;)っ";
-        clearTimeout(titleTime)
-    } else {
-        document.title = "(/≧▽≦/)你又回来啦！" ;
-        titleTime = setTimeout(function() {
-            document.title = OriginTitile
-        },
-        2000)
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>
+<script>
+document.addEventListener('visibilitychange',function(){
+    if( document.visibilityState == 'hidden' ){
+        normal_title = document.title;
+        document.title = '哎你别走吖 Σ(っ °Д °;)っ';
+    }else{
+        document.title = '(/≧▽≦/)咦！你又回来啦';
+        setTimeout(function(){
+            document.title = normal_title;
+        }, 1200)
     }
-    });
-    </script>
-
-<meting-js
-    server="netease"
-    type="song"
-    id="25704085"
-    fixed="true">
-</meting-js>
+});
+</script>
