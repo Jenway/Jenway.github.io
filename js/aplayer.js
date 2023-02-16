34c75626cbd4ee2ea37405aa373afcdfea3be30b
Jenway <JenwayHere@Gmail.com>
2023-02-16 03:29:26 +0000

    修改 post 模板 Updated By Github Actions

--- js/aplayer.js
+++ js/aplayer.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>!(function() {
+!(function() {
   var oldLoadAp = window.onload;
   window.onload = function () {
     oldLoadAp && oldLoadAp();
@@ -15,8 +15,8 @@
         {
           name: 'song1',
           artist: 'artist1',
-          url: 
-          cover: 
+          url: '/songs/song1.mp3',
+          cover: '/img/cover.jpg'
         }
       ]
     });
